18db61d590dc87e14bd08d0e9c7122e7c9dfb2e6
Lutovit <pilot_mig@bk.ru>
2021-08-18 22:00:28 +0300

    Добавьте файлы проекта.

---
+++ Config_2/AdminSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Config_2
+{
+    public class AdminSettings
+    {
+        public const string Settings = "AdminSettings";
+
+
+        public string adminName { set; get; }
+        public string adminEmail { set; get; }
+    }
+}
---
+++ Config_2/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Config_2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Config_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly AdminSettings _options;
+        
+        public HomeController(ILogger<HomeController> logger, IOptions<AdminSettings> options)
+        {
+            _logger = logger;
+            _options = options.Value;
+        }
+        
+
+        
+        public string Index()
+        {
+            string str = _options.adminEmail + "   ;   " + _options.adminName;
+            return str;
+        }
+        
+
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
